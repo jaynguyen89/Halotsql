be6def3b9c3fe07c89e3d022587a5062917c0318
Jay Nguyen <jay.nguyen.developer@gmail.com>
2025-03-05 13:49:56 +1100

    TSQL-12
    Cherry-pick Occupation post deployment, and index files

--- PostDeployment/Occupation.PostDeployment.sql
+++ PostDeployment/Occupation.PostDeployment.sql
@@ -10,28 +10,28 @@ INSERT INTO [dbo].[Occupation] (
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('373084ab978e4bd2830fd10dcc61c88c', NULL, 'Software Developer', '31c64d3a646445518929c8ba81292f12');
+) VALUES ('373084ab978e4bd2830fd10dcc61c88c', '31c64d3a646445518929c8ba81292f12', 'Software Developer', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('243ce5e547ae49bfaa7a8fe273e84c44', NULL, 'Data Analyst', '31c64d3a646445518929c8ba81292f12');
+) VALUES ('243ce5e547ae49bfaa7a8fe273e84c44', '31c64d3a646445518929c8ba81292f12', 'Data Analyst', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('26c73895716d4b2daddd584af3830efc', NULL, 'DevOps Engineer', '31c64d3a646445518929c8ba81292f12');
+) VALUES ('26c73895716d4b2daddd584af3830efc', '31c64d3a646445518929c8ba81292f12', 'DevOps Engineer', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('1e46bb1d9b664332974eabfd1b0a6d14', NULL, 'Network Engineer', '31c64d3a646445518929c8ba81292f12');
+) VALUES ('1e46bb1d9b664332974eabfd1b0a6d14', '31c64d3a646445518929c8ba81292f12', 'Network Engineer', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
@@ -45,25 +45,25 @@ INSERT INTO [dbo].[Occupation] (
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('1459b5a3e9864aa0846eeb2a386935d0', NULL, 'Photographer', '74413801520a45328c52f7c2e73160c6');
+) VALUES ('1459b5a3e9864aa0846eeb2a386935d0', '74413801520a45328c52f7c2e73160c6', 'Photographer', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('1f5ba6d1fa9549cb953ddbfdce98d246', NULL, 'Landscape Designer', '74413801520a45328c52f7c2e73160c6');
+) VALUES ('1f5ba6d1fa9549cb953ddbfdce98d246', '74413801520a45328c52f7c2e73160c6', 'Landscape Designer', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('3633d575444a46bd83dfb5864322fee8', NULL, 'Drawer & Painter', '74413801520a45328c52f7c2e73160c6');
+) VALUES ('3633d575444a46bd83dfb5864322fee8', '74413801520a45328c52f7c2e73160c6', 'Drawer & Painter', NULL);
 
 INSERT INTO [dbo].[Occupation] (
 	[Id],
 	[ParentId],
 	[Name],
 	[Description]
-) VALUES ('28bc278f165e415db60324f820dbb02d', NULL, 'Dancing Choreography', '74413801520a45328c52f7c2e73160c6');
+) VALUES ('28bc278f165e415db60324f820dbb02d', '74413801520a45328c52f7c2e73160c6', 'Dancing Choreography', NULL);
---
+++ PostDeployment/PostDeployment.Index.sql
@@ -0,0 +1,6 @@
+﻿:r .\Role.PostDeployment.sql
+:r .\Currency.PostDeployment.sql
+:r .\Interest.PostDeployment.sql
+:r .\Locality.PostDeployment.sql
+:r .\LocalityDivision.PostDeployment.sql
+:r .\Occupation.PostDeployment.sql
